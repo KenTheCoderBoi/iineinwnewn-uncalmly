698d8e6315529c9acaaed8048ca72eab64537c34
dancingcookieman <74688350+dancingcookieman@users.noreply.github.com>
2021-05-05 15:42:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+Webcam.set({
+    width:350,
+    height:300 ,
+    image_format:"png",
+    png_quality:90
+})
+    
+camera = document.getElementById("camera")
+Webcam.attach('#camera')
+ function takepic(){
+     Webcam.snap(function (data_uri) {
+         document.getElementById("output").innerHTML = "<image id='picturetaken' src="+data_uri+" >"
+     })
+ }
+ console.log("ml5 version",ml5.version)
+ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/CpYENAtgU/model.json",modelLoaded)
+ function modelLoaded(){
+     console.log("Model loaded")
+ }
+ function identify(){
+    img = document.getElementById("picturetaken")
+    classifier.classify(img,gotResult)
+}
+function gotResult(error,results){
+if(error){
+   console.error(error)
+}else{
+   console.log(results)
+   document.getElementById("object").innerHTML = results[0].label
+   //document.getElementById("accuracy").innerHTML = results[0]    
+}
+}
